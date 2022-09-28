99259d60cb345eae15211397f3199aa86ac2bceb
mzuenni <michi.zuendorf@gmail.com>
2022-09-28 13:48:16 +0200

    fix

--- math/goldenSectionSearch.cpp
+++ math/goldenSectionSearch.cpp
@@ -1,14 +1,15 @@
 ld gss(ld l, ld r, function<ld(ld)> f) {
-	ld r = (sqrtl(5.0l)-1)/2;
-	ld x1 = b - r*(b-a), x2 = a + r*(b-a);
+	ld inv = (sqrt(5.0l) - 1) / 2;
+	ld x1 = r - inv*(r-l), x2 = l + inv*(r-l);
 	ld f1 = f(x1), f2 = f(x2);
 	for (int i = 0; i < 200; i++) {
 		if (f1 < f2) { //change to > to find maximum
-			b = x2; x2 = x1; f2 = f1;
-			x1 = b - r*(b-a); f1 = f(x1);
+			u = x2; x2 = x1; f2 = f1;
+			x1 = r - inv*(r-l); f1 = f(x1);
 		} else {
-			a = x1; x1 = x2; f1 = f2;
-			x2 = a + r*(b-a); f2 = f(x2);
-	}}
-	return a;
+			l = x1; x1 = x2; f1 = f2;
+			x2 = l + inv*(r-l); f2 = f(x2);
+		}
+	}
+	return l;
 }
