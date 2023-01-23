2ce6e6376ee0c0753837b7d423bd7cc297d37220
MZuenni <michi.zuendorf@gmail.com>
2023-01-23 17:50:09 +0100

    shortened code

--- geometry/closestPair.cpp
+++ geometry/closestPair.cpp
@@ -1,7 +1,3 @@
-double squaredDist(pt a, pt b) {
-	return real(conj(a-b) * (a-b));
-}
-
 bool compY(pt a, pt b) {
 	return (imag(a) == imag(b)) ? real(a) < real(b) 
 	                            : imag(a) < imag(b);
@@ -28,7 +24,7 @@ double shortestDist(vector<pt>& pts) { // sz(pts) > 1
 			auto lower = status.lower_bound({-1.0/0.0, imag(*right) - sqrtOpt});
 			auto upper = status.upper_bound({-1.0/0.0, imag(*right) + sqrtOpt});
 			for (;lower != upper; lower++) {
-				double cand = squaredDist(*right, *lower);
+				double cand = norm(*right - *lower);
 				if (cand < opt) {
 					opt = cand;
 					sqrtOpt = sqrt(opt);
