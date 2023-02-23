63a38d41b3c2b50429d4af7b3cdffdcdd915e3b7
mzuenni <michi.zuendorf@gmail.com>
2023-02-23 18:16:53 +0100

    added more ctalan stuff

--- math/binomial.cpp
+++ math/binomial.cpp
@@ -1,8 +1,7 @@
 ll calc_binom(ll n, ll k) {
-	ll r = 1, d;
 	if (k > n) return 0;
-	// Reihenfolge garantiert Teilbarkeit
-	for (d = 1; d <= k; d++) {
+	ll r = 1;
+	for (ll d = 1; d <= k; d++) {// Reihenfolge => Teilbarkeit
 		r *= n--;
 		r /= d;
 	}
--- math/binomial3.cpp
+++ math/binomial3.cpp
@@ -1,5 +1,6 @@
 ll calc_binom(ll n, ll k, ll p) {
-	if (n >= p || k > n) return 0;
+	assert(n < p) //wichtig: sonst falsch!
+	if (k > n) return 0;
 	ll x = k % 2 != 0 ? p-1 : 1;
 	for (ll c = p-1; c > n; c--) {
 		x *= c - k; x %= p;
