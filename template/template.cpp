7c5258421f636dfd59721ea943902336d8b5387f
mzuenni <michi.zuendorf@gmail.com>
2023-02-23 18:31:24 +0100

    improved template

--- template/template.cpp
+++ template/template.cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define fora(i, n) for (int i = 0; i < n; ++i)
 #define forb(i, n) for (int i = 1; i <= n; ++i)
-#define all(x) begin(x), end(x) 
-#define sz(x) (ll)(x).size()
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)size(x)
 #define _ << " " <<
 #define debug(x) #x << " = " << (x)
- 
+
 using ll = long long;
 using ld = long double;
-using pii = std::pair<int, int>;
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-} 
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+}
