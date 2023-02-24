208388a728ee7d7ea8f33952a15bb71c27740e50
Noobie99 <noob999noob999@gmail.com>
2023-02-24 17:19:27 +0100

    template

--- template/template.cpp
+++ template/template.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 #define fora(i, n) for (int i = 0; i < n; ++i)
 #define forb(i, n) for (int i = 1; i <= n; ++i)
-#define all(x) begin(x), end(x)
-#define sz(x) (ll)(x).size()
+#define all(x) ::begin(x), ::end(x)
+#define sz(x) (ll)::size(x)
 #define _ << " " <<
 #define debug(x) #x << " = " << (x)
 
