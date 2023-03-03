61710d33fddc342904c69b955cc3b5a363473d1e
mzuenni <michi.zuendorf@gmail.com>
2023-03-03 23:45:53 +0100

    fixed sos

--- other/sos.cpp
+++ other/sos.cpp
@@ -2,5 +2,5 @@ vector<ll> res(in);
 for (int i = 1; i < sz(res); i *= 2) {
 	for (int mask = 0; mask < sz(res); mask++){
 		if (mask & i) {
-			F[mask] += F[mask ^ i];
+			res[mask] += res[mask ^ i];
 }}}
