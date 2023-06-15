e3a5bfeab3faa50fee3c3404fc828ebe751f961e
Noobie99 <noob999noob999@gmail.com>
2023-06-15 10:40:15 +0200

    update template

--- template/template.cpp
+++ template/template.cpp
@@ -5,13 +5,12 @@ using namespace std;
 #define tsolve int t; cin >> t; while(t--) solve
 #define all(x) ::begin(x), ::end(x)
 #define sz(x) (ll)::size(x)
-#define debug(x) cerr << __LINE__ << #x << ": " << (x) << endl
+#define debug(x) cerr << __LINE__ << ": "#x" = " << (x) << endl
  
 void solve() {}
  
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
+	cin.tie(0)->sync_with_stdio(false);
 	cout << setprecision(16);
 	solve();
 }
