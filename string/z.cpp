177e2de6ed58fe3fa930c19a097eaf38e31b7043
Noobie99 <noob999noob999@gmail.com>
2023-09-15 17:07:23 +0200

    slightly shorten z function

--- string/z.cpp
+++ string/z.cpp
@@ -1,11 +1,10 @@
 vector<int> Z(const string& s) {
     int n = sz(s);
-    vector<int> z(n, n);
-    int x = 0, y = 0;
-    for (int i = 1; i < n; i++) {
-        z[i] = max(0, min(z[i - x], y - i + 1));
+    vector<int> z(n);
+    for (int i = 1, x = 0; i < n; i++) {
+        z[i] = max(0, min(z[i - x], x + z[x] - i));
         while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-            x = i, y = i + z[i], z[i]++;
+            x = i, z[i]++;
     }}
     return z;
 }
