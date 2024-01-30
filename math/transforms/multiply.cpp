9819b394e494bdfaaf55893a3f90b71ddb52227e
Noobie99 <noob999noob999@gmail.com>
2024-01-30 22:01:40 +0100

    add multiply function for fft

---
+++ math/transforms/multiply.cpp
@@ -0,0 +1,12 @@
+vector<ll> mul(vector<ll>& a, vector<ll>& b) {
+	int n = 1 << (__lg(sz(a) + sz(b)) + 1);
+	vector<cplx> a2(all(a)), b2(all(b));
+	a2.resize(n), b2.resize(n);
+	fft(a2), fft(b2);
+	for (int i=0; i<n; i++) a2[i] *= b2[i];
+	fft(a2, true);
+
+	vector<ll> ans(n);
+	for (int i=0; i<n; i++) ans[i] = llround(a2[i].real());
+	return ans;
+}
