631335a9c1dd0fba0e17977253d0a4b033aee59a
mzuenni <michi.zuendorf@gmail.com>
2024-02-03 14:33:16 +0100

    fix indent

--- string/z.cpp
+++ string/z.cpp
@@ -1,10 +1,10 @@
 vector<int> Z(const string& s) {
-    int n = sz(s);
-    vector<int> z(n);
-    for (int i = 1, x = 0; i < n; i++) {
-        z[i] = max(0, min(z[i - x], x + z[x] - i));
-        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-            x = i, z[i]++;
-    }}
-    return z;
+	int n = sz(s);
+	vector<int> z(n);
+	for (int i = 1, x = 0; i < n; i++) {
+		z[i] = max(0, min(z[i - x], x + z[x] - i));
+		while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+			x = i, z[i]++;
+	}}
+	return z;
 }
