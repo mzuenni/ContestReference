bc3da2b39b9e7b0c56d4e86900081bd84f1cb5ab
mzuenni <michi.zuendorf@gmail.com>
2024-07-02 16:37:45 +0200

    automaticly test all cpp files

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-g++ -std=gnu++17 template/template.cpp -I ../content/
+find . -type f -name '*.cpp' -print0 | while read -d $'\0' file
+do
+	echo "${file}:"
+    echo "compiling..."
+    g++ -std=gnu++17 "${file}" -I ../content/
+    echo "running..."
+    ./a.out
+    echo ""
+done
+
