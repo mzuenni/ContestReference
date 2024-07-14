e23807b65e2705878dc63930735e639213268021
mzuenni <michi.zuendorf@gmail.com>
2024-07-14 22:48:00 +0200

    fix?

--- content/math/transforms/multiplyBitwise.cpp
+++ content/math/transforms/multiplyBitwise.cpp
@@ -1,5 +1,6 @@
 vector<ll> mul(vector<ll> a, vector<ll> b) {
 	int n = 1 << (__lg(max(sz(a), sz(b)) - 1) + 1);
+	if (sz(a) == 1 && sz(b) == 1) n = 1;
 	a.resize(n), b.resize(n);
 	bitwiseConv(a), bitwiseConv(b);
 	for (int i=0; i<n; i++) a[i] *= b[i]; // MOD?
