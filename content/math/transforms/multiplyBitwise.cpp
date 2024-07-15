ce413e6a73cb04cc4b02d80fa7be430471ece226
Yidi <noob999noob999@gmail.com>
2024-07-15 11:51:49 +0200

    better fix

--- content/math/transforms/multiplyBitwise.cpp
+++ content/math/transforms/multiplyBitwise.cpp
@@ -1,6 +1,5 @@
 vector<ll> mul(vector<ll> a, vector<ll> b) {
-	int n = 1 << (__lg(max(sz(a), sz(b)) - 1) + 1);
-	if (sz(a) == 1 && sz(b) == 1) n = 1;
+	int n = 1 << (__lg(2 * max(sz(a), sz(b)) - 1));
 	a.resize(n), b.resize(n);
 	bitwiseConv(a), bitwiseConv(b);
 	for (int i=0; i<n; i++) a[i] *= b[i]; // MOD?
