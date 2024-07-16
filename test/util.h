f6cc8c671b1d000bf825eba43d61a30b94c68946
mzuenni <michi.zuendorf@gmail.com>
2024-07-16 18:43:39 +0200

    new tests

--- test/util.h
+++ test/util.h
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define all(x) ::begin(x), ::end(x)
-#define sz(x) (ll)::size(x)
+#define all(x) std::begin(x), std::end(x)
+#define sz(x) (ll)std::size(x)
 
 using ll = long long;
 using lll = __int128;
@@ -159,6 +159,11 @@ struct timer {
 	~timer() {
 		if (running) cerr << "timer not stopped!" << FAIL;
 	}
+
+	friend std::ostream& operator<<(std::ostream& os, const timer& t) {
+		if (t.running) cerr << "timer already running!" << FAIL;
+		return os << t.time;
+	}
 };
 
 namespace c20 {
@@ -174,3 +179,145 @@ namespace c20 {
 		return c20::detail::to_array_impl(a, std::make_index_sequence<N>{});
 	}
 }
+
+struct NoData {};
+
+template<typename W = NoData>
+struct Edge {
+	int from, to;
+	mutable W w;
+
+	Edge(int from_, int to_, W w_ = {}) : from(from_), to(to_), w(w_) {}
+
+	template<typename F>
+	void forE(int x, F&& f) const {
+		int y = from ^ to ^ x;
+		if constexpr (is_same_v<W, NoData>) f(x, y);
+		else f(x, y, w);
+	}
+
+	template<typename F>
+	void forE(F&& f) const {
+		forE(from, f);
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Edge& e) {
+		os << e.from << "->" << e.to;
+		if constexpr (!is_same_v<W, NoData>) os << ": " << e.w;
+		return os;
+	}
+};
+
+
+template<typename W = NoData, bool DIR = false, bool MULTI = false, bool LOOPS = false>
+class Graph {
+	vector<multimap<int, int>> adj;
+	vector<Edge<W>> edges;
+
+	void _addAdj(int e) {
+		adj[edges[e].from].emplace(edges[e].to, e);
+		if constexpr (!DIR && edges[e].from != edges[e].to) adj[edges[e].to].emplace(edges[e].from, e);
+	}
+public:
+
+	Graph(int n) : adj(n) {}
+
+	int m() const {return sz(edges);}
+	int n() const {return sz(adj);}
+	int deg(int x) const {return sz(adj[x]);}
+
+	bool addEdge(int from, int to, W w = {}) {
+		if (!LOOPS && from == to) return false;
+		if (!MULTI && adj[from].find(to) != adj[from].end()) return false;
+		edges.emplace_back(from, to, w);
+		_addAdj(sz(edges) - 1);
+		return true;
+	}
+
+	Graph& reverse() {
+		for (auto& e : edges) swap(e.from, e.to);
+		adj.assign(adj.size(), {});
+		for (int i = 0; i < sz(edges); i++) _addAdj(i);
+	}
+
+	Graph& shuffle() {
+		shuffle(all(edges), Random::rng);
+		if constexpr (!DIR) {
+			for (auto& e : edges) {
+				if (Random::integer(0, 2)) swap(e.from, e.to);
+			}
+		}
+		adj.assign(adj.size(), {});
+		for (int i = 0; i < sz(edges); i++) _addAdj(i);
+	}
+
+	Graph& permutate() {
+		vector<int> perm(n());
+		iota(all(perm), 0);
+		shuffle(all(perm), Random::rng);
+		for (auto& e : edges) {
+			e.from = perm[e.from];
+			e.to = perm[e.to];
+		}
+		shuffle();
+	}
+
+	template<typename F>
+	void forEdges(F&& f) {
+		for (auto e : edges) e.forE(f);
+	}
+
+	template<typename F>
+	void forOut(int x, F&& f) {
+		for (auto [_, id] : adj[x]) edges[id].forE(x, f);
+	}
+
+	Graph& tree() {
+		if (DIR) cerr << "randomTree must be undirected" << FAIL;
+		if (n() <= 1) return *this;
+		auto code = Random::integers<int>(n()-2, 0, n());
+		vector<int> count(n());
+		for (int x : code) count[x]++;
+		int current = -1;
+		int skip = -1;
+		auto getNext = [&](){
+			int oldSkip = skip;
+			skip = -1;
+			if (0 <= oldSkip and oldSkip <= current) return oldSkip;
+			for (current++; count[current] > 0; current++) {}
+			return current;
+		};
+		for (int x : code) {
+			addEdge(x,  getNext());
+			count[x]--;
+			if (count[x] == 0) skip = x;
+		}
+		addEdge(getNext(), getNext());
+		return *this;
+	}
+
+	Graph& star() {
+		for (int i = 1; i < n; i++) addEdge(0, i);
+		return *this;
+	}
+
+	Graph& path() {
+		for (int i = 1; i < n; i++) addEdge(i-1, i);
+		return *this;
+	}
+
+	Graph& erdosRenyi(int m) {
+		while (m > 0) {//this is slow...
+			int a = Random::integer(0, n());
+			int b = Random::integer(0, n());
+			if (addEdge(a, b)) m--;
+		}
+		return *this;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Graph& g) {
+		os << g.n() << " " << g.m();
+		for (auto& e : g.edges) os << endl << e;
+		return os;
+	}
+};
