9682f525a3f4c07de062f940233cec734543e8a7
Yidi <noob999noob999@gmail.com>
2025-01-28 15:32:21 +0100

    fix comments

--- content/graph/euler.cpp
+++ content/graph/euler.cpp
@@ -3,15 +3,15 @@ vector<int> cycle;
 
 void addEdge(int u, int v) {
 	adj[u].emplace_back(v, sz(adj[v]));
-	adj[v].emplace_back(u, sz(adj[u])-1); // remove for undirected
+	adj[v].emplace_back(u, sz(adj[u]) - 1); // remove for directed
 }
 
 void euler(int v) {
 	while (!adj[v].empty()) {
 		auto [u, rev] = adj[v].back();
 		adj[v].pop_back();
-		if (u == -1) continue;
-		adj[u][rev].first = -1; // remove for undirected
+		if (u == -1) continue; // remove for directed
+		adj[u][rev].first = -1; // remove for directed
 		euler(u);
 	}
 	cycle.push_back(v); // Zyklus in umgekehrter Reihenfolge.
